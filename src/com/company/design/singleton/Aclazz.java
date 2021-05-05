0a7b9c4d6220f9e38933865134a138a64a331ed4
mooh2jj@naver.com <62453668+mooh2jj@users.noreply.github.com>
2021-05-05 11:51:35 +0900

    싱글톤 패턴 구현

---
+++ src/com/company/design/singleton/Aclazz.java
@@ -0,0 +1,16 @@
+package com.company.design.singleton;
+
+import java.net.Socket;
+
+public class Aclazz {
+
+    private SocketClient socketClient;
+
+    public Aclazz(){
+        this.socketClient = SocketClient.getInstance();
+    }
+
+    public SocketClient getSocketClient() {
+        return this.socketClient;
+    }
+}
